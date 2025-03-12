95b54425b830d5666efba54f12d2f4507e0c33e6
Alexandre <aacleite@gmail.com>
2025-03-12 20:12:49 -0300

    Adicionando Arquivos de praticas01 com alterações 2

--- praticas/pratica01/ola_mundo.c
+++ praticas/pratica01/ola_mundo.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
-int main( ){
-    printf("=============================\n");
-    printf("        Ola Mundo!\n");
-    printf("Bem vindo a disciplina APC I!\n");
-    printf("=============================");
+int main( )
+{
+    printf("+=================================+\n");
+    printf("|           Ola Mundo!            |\n");
+    printf("|  Bem vindo a disciplina APC I!  |\n");
+    printf("+=================================+");
+    printf("\n+=================================+");
+    printf("\n|   Com esse programa voce esta   |");
+    printf("\n|  habilitado para hakear a NASA! |");
+    printf("\n+=================================+\n");
+
     return 0;
 }
