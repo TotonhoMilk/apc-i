8e623f4775a8cf673ab8be6f3ccac8e4855c58b8
Alexandre <aacleite@gmail.com>
2025-03-12 20:50:10 -0300

    Adicionando arquivos pratica01 com inovaçoes01

--- praticas/pratica01/ola_mundo.c
+++ praticas/pratica01/ola_mundo.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+int resultado, num1, num2;
+
+int soma (int a, int b){
+    int res = a + b;
+    return res;
+}
+
 int main( )
 {
     printf("+=================================+\n");
@@ -10,6 +17,20 @@ int main( )
     printf("\n|   Com esse programa voce esta   |");
     printf("\n|  habilitado para hakear a NASA! |");
     printf("\n+=================================+\n");
+
+    printf("\n+=================================+");
+    printf("\n  Digite um numero ----->");
+    scanf("%d", &num1);
+    printf("\n  Digite outro numero --> ");
+    scanf("%d", &num2);
+
+    resultado = soma(num1, num2);
+
+    printf("  resultado --------------> %d\n", resultado);
+    printf("\n+=================================+");
+
+    
+    //printf ("%d", resultado);
 
     return 0;
 }
