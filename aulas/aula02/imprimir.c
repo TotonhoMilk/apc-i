31dcae568b39f37af40ee20718307f66d43b280e
Alexandre <aacleite@gmail.com>
2025-03-12 21:50:04 -0300

    codigos da aula 2

---
+++ aulas/aula02/imprimir.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main ( ){
+    printf("\t\t\t-----------------------------\n");
+    printf("\t\t\t   Boa Noite, Margarida!\n");
+    printf("\t\t\t   Estamos aprendendo C!\n");
+    printf("\t\t\t  Invadindo o site do IESB!\n");
+    printf("\t\t\t-----------------------------\n");
+
+    printf("\n Barra n pula a linha!");
+    printf("\n Barra t da um tab");
+
+    return 0;
+}
